02c026c5441342ba63180e84c08e9566609f81ec
ersincodes <ersin.bahar@bilgeadam.com>
2024-02-16 20:04:47 +0300

    LoginPages dark mode edited, fixed.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,11 @@ import LoginForm from "../features/authentication/LoginForm";
 import styled from "styled-components";
 import Logo from "../ui/Logo";
 import Heading from "../ui/Heading";
+import { useDarkMode } from "../context/DarkModeContext"; // Adjust the import path as necessary
 
 const LoginLayout = styled.main`
   min-height: 100vh;
-  background-color: #18212f;
+  background-color: ${(props) => (props.isDarkMode ? "#18212f" : "#fff")};
   display: grid;
   grid-template-columns: 48rem;
   align-content: center;
@@ -14,10 +15,12 @@ const LoginLayout = styled.main`
 `;
 
 function Login() {
+  const { isDarkMode } = useDarkMode();
+
   return (
-    <LoginLayout>
+    <LoginLayout isDarkMode={isDarkMode}>
       <Logo />
-      <Heading as="h1"> Log in to your account</Heading>
+      <Heading as="h1">Log in to your account</Heading>
       <LoginForm />
     </LoginLayout>
   );
